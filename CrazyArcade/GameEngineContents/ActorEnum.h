79dece853daf849d611951e15308b622183f4177
Kim Tae Wook <gmill0411@naver.com>
2023-07-21 19:21:08 +0900

    김태욱 : ActorEnum 내용 커밋

--- CrazyArcade/GameEngineContents/ActorEnum.h
+++ CrazyArcade/GameEngineContents/ActorEnum.h
@@ -1,1 +1,22 @@
 #pragma once
+
+enum class CharacterState
+{
+	Wait,
+	Move,
+	Ready,
+	FlashLong,
+	FlashShort,
+	Trap,
+	Live,
+	Die,
+	Jump
+};
+
+enum class ActorDir
+{
+	Left,
+	Right,
+	Up,
+	Down,
+};
