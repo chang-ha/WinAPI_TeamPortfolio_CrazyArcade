ecb8fa4ecda2e0c601e038f29426333378b6b60b
김태훈 <hyri0510@gmail.com>
2023-07-30 03:06:02 +0900

    김태훈 : [feature] 타일 선택 기능 업로드

--- CrazyArcade/GameEngineContents/MapEditor.cpp
+++ CrazyArcade/GameEngineContents/MapEditor.cpp
@@ -1,8 +1,10 @@
 ﻿#include <GameEngineBase/GameEnginePath.h>
 #include <GameEnginePlatform/GameEngineWindow.h>
+#include <GameEnginePlatform/GameEngineWindowTexture.h>
 #include <GameEngineCore/GameEngineRenderer.h>
 #include <GameEngineCore/TileMap.h>
 #include <GameEngineCore/ResourcesManager.h>
+#include <GameEngineCore/GameEngineSprite.h>
 #include <GameEnginePlatform/GameEngineInput.h>
 
 #include "MapEditor.h"
@@ -12,6 +14,8 @@
 #include "BackGround.h"
 #include "TileSelect.h"
 #include "GlobalLoad.h"
+#include "Button.h"
+
 
 MapEditor::MapEditor()
 {
@@ -79,9 +83,182 @@ void MapEditor::Start()
 		}
 	}
 
+
+	InitTiles();
+
+	CurrentSelectTileType = SelectTileType::Ground;
+
+	GameEngineWindowTexture* GroundsTexture = ResourcesManager::GetInst().FindTexture("Grounds.bmp");
+	if (nullptr == GroundsTexture)
+	{
+		MsgBoxAssert("스프라이트를 불러오지 못했습니다.");
+		return;
+	}
+
+ 	float4 TextureScale = GroundsTexture->GetScale();
+	int TilesXCount = static_cast<int>(TextureScale.X / GlobalValue::MapTileSize.X);
+	int TilesYCount = static_cast<int>(TextureScale.Y / GlobalValue::MapTileSize.Y);
+	CurSelectTilesCount  = TilesXCount * TilesYCount;
+
+	for (int TilesTexture = 0; TilesTexture < CurSelectTilesCount ; TilesTexture++)
+	{
+		SelectView->SetTile(TilesTexture % SelectViewSize_X, TilesTexture / SelectViewSize_X, TilesTexture, SelectView_StartPos);
+	}
+
+	LoadButton();
+
+
 	// 타일정보 벡터 초기화
 	TilesInfo.assign(GlobalValue::MapTileIndex_Y, (std::vector<int>(GlobalValue::MapTileIndex_X, 0)));
 }
+
+void MapEditor::InitTiles()
+{
+	VecTileSpriteFileName.resize(static_cast<int>(SelectTileType::Max));
+
+	VecTileSpriteFileName[static_cast<int>(SelectTileType::Ground)] = "Grounds.bmp";
+	VecTileSpriteFileName[static_cast<int>(SelectTileType::ImmovableBlock)] = "ImmovableBlocks.bmp";
+	VecTileSpriteFileName[static_cast<int>(SelectTileType::MovableBlock)] = "MovableBlocks.bmp";
+	//VecTileSpriteFileName[static_cast<int>(SelectTileType::Structure)] = "Structures.bmp";
+}
+
+
+void MapEditor::LoadButton()
+{
+	VecButton.resize(static_cast<int>(MapEditorButtonState::Max));
+
+	Button* NextButton = CreateActor<Button>(UpdateOrder::UI);
+	if (nullptr == NextButton)
+	{
+		MsgBoxAssert("Prev 버튼을 생성하지 못했습니다.");
+		return;
+	}
+
+	NextButton->setRenderer(RenderOrder::FirstElementUI);
+	NextButton->setButtonTexture(ButtonState::Normal, "Button_Next_Normal.bmp", "Resources\\Textures\\UI\\Button", 1, 1);
+	NextButton->setButtonTexture(ButtonState::Click, "Button_Next_Click.bmp", "Resources\\Textures\\UI\\Button", 1, 1);
+	NextButton->setButtonTexture(ButtonState::Hover, "Button_Next_Normal.bmp", "Resources\\Textures\\UI\\Button", 1, 1);
+
+	//ButtonPtr->setButtonSound(ButtonEventState::Click, )
+
+	NextButton->setCallback<MapEditor>(ButtonEventState::Click, this, &MapEditor::ClickNextButton);
+
+	float4 SelectView_EndXPos = SelectView_StartPos + 
+		float4{ 0.0f , static_cast<float>(SelectViewSize_Y) * GlobalValue::MapTileSize.Y };
+
+	float4 NextButtonPos = SelectView_EndXPos + LocalNextButtonStartPos;
+
+	NextButton->setButtonPos(NextButtonPos);
+	VecButton[static_cast<int>(MapEditorButtonState::Next)] = NextButton;
+
+
+
+	Button* PrevButton = CreateActor<Button>(UpdateOrder::UI);
+	if (nullptr == PrevButton)
+	{
+		MsgBoxAssert("Prev 버튼을 생성하지 못했습니다.");
+		return;
+	}
+
+	PrevButton->setRenderer(RenderOrder::FirstElementUI);
+	PrevButton->setButtonTexture(ButtonState::Normal, "Button_Prev_Normal.bmp", "Resources\\Textures\\UI\\Button", 1, 1);
+	PrevButton->setButtonTexture(ButtonState::Click, "Button_Prev_Click.bmp", "Resources\\Textures\\UI\\Button", 1, 1);
+	PrevButton->setButtonTexture(ButtonState::Hover, "Button_Prev_Normal.bmp", "Resources\\Textures\\UI\\Button", 1, 1);
+
+	//ButtonPtr->setButtonSound(ButtonEventState::Click, )
+
+	PrevButton->setCallback<MapEditor>(ButtonEventState::Click, this, &MapEditor::ClickPrevButton);
+
+	GameEngineSprite* PrevButtonSprite = ResourcesManager::GetInst().FindSprite("Button_Prev_Normal.bmp");
+	if (nullptr == PrevButtonSprite)
+	{
+		MsgBoxAssert("스프라이트를 불러오지 못했습니다.");
+		return;
+	}
+
+	float4 ButtonRenderScale = PrevButtonSprite->GetSprite(0).RenderScale;
+
+
+	float4 PrevButtonPos = SelectView_EndXPos + 
+		float4{ static_cast<float>(SelectViewSize_X) * GlobalValue::MapTileSize.X - LocalNextButtonStartPos.X - ButtonRenderScale.X, LocalNextButtonStartPos.Y };
+
+	PrevButton->setButtonPos(PrevButtonPos);
+	VecButton[static_cast<int>(MapEditorButtonState::Prev)] = PrevButton;
+}
+
+
+
+void MapEditor::ClickPrevButton()
+{
+	ClearSelectViewTiles();
+
+	int IntTileType = static_cast<int>(CurrentSelectTileType) - 1;
+	if (-1 == IntTileType)
+	{
+		CurrentSelectTileType = SelectTileType::MovableBlock;
+	}
+	else
+	{
+		CurrentSelectTileType = static_cast<SelectTileType>(IntTileType);
+	}
+
+	ChangeSelectViewTile();
+
+	for (int TilesTexture = 0; TilesTexture < CurSelectTilesCount; TilesTexture++)
+	{
+		SelectView->SetTileToSprite(
+			TilesTexture % SelectViewSize_X, TilesTexture / SelectViewSize_X,
+			VecTileSpriteFileName[static_cast<int>(CurrentSelectTileType)],
+			TilesTexture, SelectView_StartPos);
+	}
+}
+
+void MapEditor::ClickNextButton()
+{
+	ClearSelectViewTiles();
+
+	int IntTileType = static_cast<int>(CurrentSelectTileType) + 1;
+	CurrentSelectTileType = static_cast<SelectTileType>(IntTileType);
+	if (SelectTileType::Max == CurrentSelectTileType)
+	{
+		CurrentSelectTileType = SelectTileType::Ground;
+	}
+
+	ChangeSelectViewTile();
+
+	for (int TilesTexture = 0; TilesTexture < CurSelectTilesCount; TilesTexture++)
+	{
+		SelectView->SetTileToSprite(
+			TilesTexture % SelectViewSize_X, TilesTexture / SelectViewSize_X,
+			VecTileSpriteFileName[static_cast<int>(CurrentSelectTileType)], 
+			TilesTexture, SelectView_StartPos);
+	}
+}
+
+void MapEditor::ClearSelectViewTiles()
+{
+	for (int TilesTexture = 0; TilesTexture < SelectViewSize_X * SelectViewSize_Y; TilesTexture++)
+	{
+		SelectView->SetTile(TilesTexture % SelectViewSize_X, TilesTexture / SelectViewSize_X, 0, SelectView_StartPos);
+	}
+}
+
+void MapEditor::ChangeSelectViewTile()
+{
+	GameEngineWindowTexture* GroundsTexture = 
+		ResourcesManager::GetInst().FindTexture(VecTileSpriteFileName[static_cast<int>(CurrentSelectTileType)]);
+	if (nullptr == GroundsTexture)
+	{
+		MsgBoxAssert("스프라이트를 불러오지 못했습니다.");
+		return;
+	}
+
+	float4 TextureScale = GroundsTexture->GetScale();
+	int TilesXCount = static_cast<int>(TextureScale.X / GlobalValue::MapTileSize.X);
+	int TilesYCount = static_cast<int>(TextureScale.Y / GlobalValue::MapTileSize.Y);
+	CurSelectTilesCount  = TilesXCount * TilesYCount;
+}
+
 
 void MapEditor::Update(float _Delta)
 {
@@ -109,6 +286,11 @@ void MapEditor::Update(float _Delta)
 		SelectedTile->Off();
 	}
 
+
+	UpdateDrawingTileUI();
+
+
+
 	// 임시 타일 변경 기능
 	if (true == GameEngineInput::IsDown('0'))
 	{
@@ -132,9 +314,64 @@ void MapEditor::Update(float _Delta)
 	}
 }
 
+void MapEditor::UpdateDrawingTileUI()
+{
+	if (true == MouseInDrawingTileMap())
+	{
+		SelectedTile->On();
+		int CurDrawingTileIndex_X = int(CurMousePos.X - SelectView_StartPos.X) / 40;
+		int CurDrawingTileIndex_Y = int(CurMousePos.Y - SelectView_StartPos.Y) / 40;
+
+		SelectedTile->SetPos({
+			SelectView_StartPos.X + (GlobalValue::MapTileSize.X * CurDrawingTileIndex_X) + GlobalValue::MapTileSize.hX(),
+			SelectView_StartPos.Y + (GlobalValue::MapTileSize.Y * CurDrawingTileIndex_Y) + GlobalValue::MapTileSize.hY() });
+
+		if (true == GameEngineInput::IsDown(VK_LBUTTON))
+		{
+			int TemporaryCurSelectedTileType = CurDrawingTileIndex_Y * 3 + CurDrawingTileIndex_X;
+
+			if (TemporaryCurSelectedTileType < CurSelectTilesCount )
+			{
+				CurSelectedTileType = TemporaryCurSelectedTileType;
+			}
+		}
+	}
+	else
+	{
+		SelectedTile->Off();
+	}
+}
+
+
+
 void MapEditor::Render(float _Delta)
 {
+	if (false == ContentLevel::DebugValue)
+	{
+		return;
+	}
 
+	GameEngineWindowTexture* BackBufferPtr = GameEngineWindow::MainWindow.GetBackBuffer();
+	if (nullptr == BackBufferPtr)
+	{
+		MsgBoxAssert("버퍼를 불러오지 못했습니다.");
+		return;
+	}
+
+	HDC dc = BackBufferPtr->GetImageDC();
+	if (nullptr == dc)
+	{
+		MsgBoxAssert("HDC를 불러오지 못했습니다.");
+		return;
+	}
+
+
+
+	{
+		std::string Text = "";
+
+		TextOutA(dc, 0, 0, Text.c_str(), static_cast<int>(Text.size()));
+	}
 }
 
 bool MapEditor::MouseInTileMap()
@@ -143,6 +380,19 @@ bool MapEditor::MouseInTileMap()
 		CurMousePos.Y > GlobalValue::TileStartPos.Y &&
 		CurMousePos.X < GlobalValue::TileStartPos.X + (GlobalValue::MapTileSize.X * GlobalValue::MapTileIndex_X) &&
 		CurMousePos.Y < GlobalValue::TileStartPos.Y + (GlobalValue::MapTileSize.Y * GlobalValue::MapTileIndex_Y))
+	{
+		return true;
+	}
+
+	return false;
+}
+
+bool MapEditor::MouseInDrawingTileMap()
+{
+	if (CurMousePos.X > SelectView_StartPos.X &&
+		CurMousePos.Y > SelectView_StartPos.Y &&
+		CurMousePos.X < SelectView_StartPos.X + (GlobalValue::MapTileSize.X * SelectViewSize_X) &&
+		CurMousePos.Y < SelectView_StartPos.Y + (GlobalValue::MapTileSize.Y * SelectViewSize_Y))
 	{
 		return true;
 	}
--- CrazyArcade/GameEngineContents/MapEditor.h
+++ CrazyArcade/GameEngineContents/MapEditor.h
@@ -1,7 +1,30 @@
 #pragma once
 #include "ContentLevel.h"
 #include <vector>
+
+
+enum class MapEditorButtonState
+{
+	Prev,
+	Next,
+	Max,
+};
+
+enum class SelectTileType
+{
+	Ground,
+	ImmovableBlock,
+	MovableBlock,
+	//Structure,
+	//Item,
+	//Unit,
+	Max,
+};
+
+
+
 class TileMap;
+class Button;
 class MapEditor : public ContentLevel
 {
 public:
@@ -25,6 +48,7 @@ protected:
 private:
 
 	bool MouseInTileMap();
+	bool MouseInDrawingTileMap();
 
 	// Const Value
 	const float4 SelectView_StartPos = { 655.0f, 100.0f };
@@ -46,5 +70,29 @@ private:
 	TileMap* SelectView = nullptr;
 
 	std::vector<std::vector<int>>TilesInfo;
+
+
+private:
+	std::vector<std::string> VecTileSpriteFileName;
+
+
+	int CurSelectTilesCount = 0;
+
+	void UpdateDrawingTileUI();
+	
+	void InitTiles();
+	void LoadButton();
+	std::vector<Button*> VecButton;
+
+	float4 LocalNextButtonStartPos = float4{ 10.0f , 30.0f };
+
+	SelectTileType CurrentSelectTileType = SelectTileType::Max;
+
+	void ClickPrevButton();
+	void ClickNextButton();
+
+	void ClearSelectViewTiles();
+	void ChangeSelectViewTile();
+
 };
 
