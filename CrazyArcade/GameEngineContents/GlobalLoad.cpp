7d0df8c8071e30a48d498f3453b17e687cf4176e
김태훈 <hyri0510@gmail.com>
2023-08-01 21:22:20 +0900

    Revert "김서영 : 물폭탄 리소스 경로 변경"
    This reverts commit 9564f98dc98ec64677d81fd99c728091935f981e.

--- CrazyArcade/GameEngineContents/GlobalLoad.cpp
+++ CrazyArcade/GameEngineContents/GlobalLoad.cpp
@@ -21,6 +21,6 @@ void GlobalLoad::TileTextureLoad()
 		GlobalUtils::SpriteFileLoad("ImMovableBlocks.bmp", "Resources\\Textures\\Tile", ImmovableBlockMaxIndex + 1, 1);
 		GlobalUtils::SpriteFileLoad("MovableBlocks.bmp", "Resources\\Textures\\Tile", MovableBlockMaxIndex + 1, 1);
 		GlobalUtils::SpriteFileLoad("EraseTile.bmp", "Resources\\Textures\\Tile", 1, 1);
-		GlobalUtils::SpriteFileLoad("Bubble.bmp", "Resources\\Textures\\Tile\\Bubble", 3, 1);
+		GlobalUtils::SpriteFileLoad("Bubble.bmp", "Resources\\Textures\\Tile", 3, 1);
 	}
 }
