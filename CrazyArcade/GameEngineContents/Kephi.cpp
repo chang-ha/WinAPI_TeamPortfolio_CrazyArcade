5233ab41ed3e304ae3212d8ff9ea03323731025a
TaeWook <ktwisland111@gmail.com>
2023-08-09 13:10:29 +0900

    김태욱 : Resource 임시 추가 및 다른 캐릭터들 애니메이션 생성

--- CrazyArcade/GameEngineContents/Kephi.cpp
+++ CrazyArcade/GameEngineContents/Kephi.cpp
@@ -19,6 +19,7 @@ Kephi::~Kephi()
 void Kephi::Start()
 {
 	BaseCharacter::Start();
+	GlobalUtils::SpriteFileLoad("Kephi_Ready.Bmp", "Resources\\Textures\\Character\\Kephi\\", 17, 1);
 	GlobalUtils::SpriteFileLoad("Kephi_Idle_Up.Bmp", "Resources\\Textures\\Character\\Kephi\\", 1, 1);
 	GlobalUtils::SpriteFileLoad("Kephi_Idle_Down.Bmp", "Resources\\Textures\\Character\\Kephi\\", 1, 1);
 	GlobalUtils::SpriteFileLoad("Kephi_Idle_Left.Bmp", "Resources\\Textures\\Character\\Kephi\\", 1, 1);
@@ -27,8 +28,15 @@ void Kephi::Start()
 	GlobalUtils::SpriteFileLoad("Kephi_Right.Bmp", "Resources\\Textures\\Character\\Kephi\\", 6, 1);
 	GlobalUtils::SpriteFileLoad("Kephi_Up.Bmp", "Resources\\Textures\\Character\\Kephi\\", 8, 1);
 	GlobalUtils::SpriteFileLoad("Kephi_Down.Bmp", "Resources\\Textures\\Character\\Kephi\\", 8, 1);
+	GlobalUtils::SpriteFileLoad("Kephi_Bubble.Bmp", "Resources\\Textures\\Character\\Kephi\\", 4, 1);
+	GlobalUtils::SpriteFileLoad("Kephi_FlashLong.Bmp", "Resources\\Textures\\Character\\Kephi\\", 4, 1);
+	GlobalUtils::SpriteFileLoad("Kephi_FlashShort.Bmp", "Resources\\Textures\\Character\\Kephi\\", 2, 1);
+	GlobalUtils::SpriteFileLoad("Kephi_Live.Bmp", "Resources\\Textures\\Character\\Kephi\\", 5, 1);
+	GlobalUtils::SpriteFileLoad("Kephi_Die.Bmp", "Resources\\Textures\\Character\\Kephi\\", 13, 1);
+	GlobalUtils::SpriteFileLoad("Kephi_Jump.Bmp", "Resources\\Textures\\Character\\Kephi\\", 8, 1);
 
 	MainRenderer = CreateRenderer(RenderOrder::MapObject);
+	MainRenderer->CreateAnimation("Kephi_Ready", "Kephi_Ready.Bmp");
 	MainRenderer->CreateAnimation("Kephi_Idle_Up", "Kephi_Idle_Up.Bmp");
 	MainRenderer->CreateAnimation("Kephi_Idle_Down", "Kephi_Idle_Down.Bmp");
 	MainRenderer->CreateAnimation("Kephi_Idle_Left", "Kephi_Idle_Left.Bmp");
@@ -37,8 +45,14 @@ void Kephi::Start()
 	MainRenderer->CreateAnimation("Kephi_Move_Right", "Kephi_Right.Bmp");
 	MainRenderer->CreateAnimation("Kephi_Move_Up", "Kephi_Up.Bmp");
 	MainRenderer->CreateAnimation("Kephi_Move_Down", "Kephi_Down.Bmp");
+	MainRenderer->CreateAnimation("Kephi_Bubble", "Kephi_Bubble.Bmp");
+	MainRenderer->CreateAnimation("Kephi_FlashLong", "Kephi_FlashLong.Bmp");
+	MainRenderer->CreateAnimation("Kephi_FlashShort", "Kephi_FlashShort.Bmp");
+	MainRenderer->CreateAnimation("Kephi_Live", "Kephi_Live.Bmp");
+	MainRenderer->CreateAnimation("Kephi_Die", "Kephi_Die.Bmp");
+	MainRenderer->CreateAnimation("Kephi_Jump", "Kephi_Jump.Bmp", -1, -1, 0.3f, true);
 
-	ChangeState(CharacterState::Idle);
+	ChangeState(CharacterState::Ready);
 }
 
 void Kephi::ChangeAnimationState(const std::string& _StateName)
