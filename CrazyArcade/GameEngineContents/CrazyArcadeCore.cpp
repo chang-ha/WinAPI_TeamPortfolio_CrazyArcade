0d0b528f483df53699fd87862dcfdcfcb8262aa1
ddeoyoung <ksysnake3690@gmail.com>
2023-08-11 21:22:15 +0900

    김서영 : 타이머 버그 수정 및 가상키 추가

--- CrazyArcade/GameEngineContents/CrazyArcadeCore.cpp
+++ CrazyArcade/GameEngineContents/CrazyArcadeCore.cpp
@@ -37,7 +37,7 @@ void CrazyArcadeCore::Start()
 	GameEngineCore::CreateLevel<UITestStage>("UITestStage");
 	
 
-	GameEngineCore::ChangeLevel("Penguin_Stage3");
+	GameEngineCore::ChangeLevel("Penguin_Stage1");
 }
 
 void CrazyArcadeCore::Update(float _Delta)
