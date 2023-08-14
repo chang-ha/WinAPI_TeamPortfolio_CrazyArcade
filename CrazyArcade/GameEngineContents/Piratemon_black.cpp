d1c7ad88fa993003385ed5bfa74aaa28709c2786
Kim Tae Wook <gmill0411@naver.com>
2023-08-14 11:11:24 +0900

    김태욱 : 스프라이트 로드 및 애니메이션 제작

--- CrazyArcade/GameEngineContents/Piratemon_black.cpp
+++ CrazyArcade/GameEngineContents/Piratemon_black.cpp
@@ -22,6 +22,28 @@ Piratemon_black::~Piratemon_black()
 void Piratemon_black::Start()
 {
 	BaseMonster::Start();
+
+	GlobalUtils::SpriteFileLoad("Pirate_Black_Idle_Down.Bmp", "Resources\\Textures\\Monster\\Pirate\\Black\\", 1, 1);
+	GlobalUtils::SpriteFileLoad("Pirate_Black_Idle_Up.Bmp", "Resources\\Textures\\Monster\\Pirate\\Black\\", 1, 1);
+	GlobalUtils::SpriteFileLoad("Pirate_Black_Idle_Right.Bmp", "Resources\\Textures\\Monster\\Pirate\\Black\\", 1, 1);
+	GlobalUtils::SpriteFileLoad("Pirate_Black_Idle_Left.Bmp", "Resources\\Textures\\Monster\\Pirate\\Black\\", 1, 1);
+	GlobalUtils::SpriteFileLoad("Pirate_Black_Move_Down.Bmp", "Resources\\Textures\\Monster\\Pirate\\Black\\", 2, 1);
+	GlobalUtils::SpriteFileLoad("Pirate_Black_Move_Up.Bmp", "Resources\\Textures\\Monster\\Pirate\\Black\\", 2, 1);
+	GlobalUtils::SpriteFileLoad("Pirate_Black_Move_Right.Bmp", "Resources\\Textures\\Monster\\Pirate\\Black\\", 2, 1);
+	GlobalUtils::SpriteFileLoad("Pirate_Black_Move_Left.Bmp", "Resources\\Textures\\Monster\\Pirate\\Black\\", 2, 1);
+	GlobalUtils::SpriteFileLoad("Pirate_Black_Death.Bmp", "Resources\\Textures\\Monster\\Pirate\\Black\\", 4, 1);
+
+	MainRenderer = CreateRenderer(RenderOrder::MapObject);
+
+	MainRenderer->CreateAnimation("PiratemonBlack_Idle_Down", "Pirate_Black_Idle_Down.Bmp");
+	MainRenderer->CreateAnimation("PiratemonBlack_Idle_Up", "Pirate_Black_Idle_Up.Bmp");
+	MainRenderer->CreateAnimation("PiratemonBlack_Idle_Right", "Pirate_Black_Idle_Right.Bmp");
+	MainRenderer->CreateAnimation("PiratemonBlack_Idle_Left", "Pirate_Black_Idle_Left.Bmp");
+	MainRenderer->CreateAnimation("PiratemonBlack_Move_Down", "Pirate_Black_Move_Down.Bmp");
+	MainRenderer->CreateAnimation("PiratemonBlack_Move_Up", "Pirate_Black_Move_Up.Bmp");
+	MainRenderer->CreateAnimation("PiratemonBlack_Move_Right", "Pirate_Black_Move_Right.Bmp");
+	MainRenderer->CreateAnimation("PiratemonBlack_Move_Left", "Pirate_Black_Move_Left.Bmp");
+	MainRenderer->CreateAnimation("PiratemonBlack_Death", "Pirate_Black_Death.Bmp");
 }
 
 void Piratemon_black::ChangeAnimationState(const std::string& _StateName)
--- CrazyArcade/GameEngineContents/Piratemon_red.cpp
+++ CrazyArcade/GameEngineContents/Piratemon_red.cpp
@@ -22,6 +22,30 @@ Piratemon_red::~Piratemon_red()
 void Piratemon_red::Start()
 {
 	BaseMonster::Start();
+
+	GlobalUtils::SpriteFileLoad("Pirate_Red_Idle_Down.Bmp", "Resources\\Textures\\Monster\\Pirate\\Red\\", 1, 1);
+	GlobalUtils::SpriteFileLoad("Pirate_Red_Idle_Up.Bmp", "Resources\\Textures\\Monster\\Pirate\\Red\\", 1, 1);
+	GlobalUtils::SpriteFileLoad("Pirate_Red_Idle_Right.Bmp", "Resources\\Textures\\Monster\\Pirate\\Red\\", 1, 1);
+	GlobalUtils::SpriteFileLoad("Pirate_Red_Idle_Left.Bmp", "Resources\\Textures\\Monster\\Pirate\\Red\\", 1, 1);
+	GlobalUtils::SpriteFileLoad("Pirate_Red_Move_Down.Bmp", "Resources\\Textures\\Monster\\Pirate\\Red\\", 2, 1);
+	GlobalUtils::SpriteFileLoad("Pirate_Red_Move_Up.Bmp", "Resources\\Textures\\Monster\\Pirate\\Red\\", 2, 1);
+	GlobalUtils::SpriteFileLoad("Pirate_Red_Move_Right.Bmp", "Resources\\Textures\\Monster\\Pirate\\Red\\", 2, 1);
+	GlobalUtils::SpriteFileLoad("Pirate_Red_Move_Left.Bmp", "Resources\\Textures\\Monster\\Pirate\\Red\\", 2, 1);
+	GlobalUtils::SpriteFileLoad("Pirate_Red_Death.Bmp", "Resources\\Textures\\Monster\\Pirate\\Red\\", 4, 1);
+
+
+	MainRenderer = CreateRenderer(RenderOrder::MapObject);
+
+	MainRenderer->CreateAnimation("PiratemonRed_Idle_Down", "Pirate_Red_Idle_Down.Bmp");
+	MainRenderer->CreateAnimation("PiratemonRed_Idle_Up", "Pirate_Red_Idle_Up.Bmp");
+	MainRenderer->CreateAnimation("PiratemonRed_Idle_Right", "Pirate_Red_Idle_Right.Bmp");
+	MainRenderer->CreateAnimation("PiratemonRed_Idle_Left", "Pirate_Red_Idle_Left.Bmp");
+	MainRenderer->CreateAnimation("PiratemonRed_Move_Down", "Pirate_Red_Move_Down.Bmp");
+	MainRenderer->CreateAnimation("PiratemonRed_Move_Up", "Pirate_Red_Move_Up.Bmp");
+	MainRenderer->CreateAnimation("PiratemonRed_Move_Right", "Pirate_Red_Move_Right.Bmp");
+	MainRenderer->CreateAnimation("PiratemonRed_Move_Left", "Pirate_Red_Move_Left.Bmp");
+	MainRenderer->CreateAnimation("PiratemonRed_Death", "Pirate_Red_Death.Bmp");
+
 }
 
 void Piratemon_red::ChangeAnimationState(const std::string& _StateName)
