68b5de3eca54fdb7e4b6ddd72c0ab8a716bfc63b
Kim Tae Wook <gmill0411@naver.com>
2023-08-17 21:46:52 +0900

    김태욱 : 불필요한 헤더 제거

--- CrazyArcade/GameEngineContents/Penguin_Stage1.cpp
+++ CrazyArcade/GameEngineContents/Penguin_Stage1.cpp
@@ -5,7 +5,6 @@
 #include "Piratemon_black.h"
 #include "Piratemon_red.h"
 #include <GameEngineCore/TileMap.h>
-#include <GameEnginePlatform/GameEngineWindow.h>
 
 Penguin_Stage1::Penguin_Stage1()
 {
